0d21331913a528b1f7154dbb0a18ab88260d8d50
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2023-01-05 12:57:18 +0530

    Django essential training

---
+++ 01_DjangoEssentialTraining/notes/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Create your views here.
+from .forms import NotesForm
+from .models import Notes
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
+
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+class NotesCreateView(LoginRequiredMixin, CreateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+class NotesListView(LoginRequiredMixin, ListView):
+    model = Notes
+    context_object_name = 'notes'
+    template_name = 'notes/notes_list.html'
+    login_url = '/admin'
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
